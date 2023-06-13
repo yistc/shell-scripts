93f80818e07b8d4525e59720ee431fb410e90ed7
yistc <32131698+yistc@users.noreply.github.com>
2023-06-13 19:36:58 +0800

    fix dev_init.sh

--- init/dev_init.sh
+++ init/dev_init.sh
@@ -7,7 +7,7 @@ trap _exit INT QUIT TERM
 # Colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m
+YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
