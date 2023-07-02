6d2f42986757b8bc061dffea22b0d67e243944c3
yistc <32131698+yistc@users.noreply.github.com>
2023-07-03 00:21:09 +0800

    docker

--- install/docker.sh
+++ install/docker.sh
@@ -16,8 +16,35 @@ NC='\033[0m' # No Color
 
 OS=$(uname -s) # Linux, FreeBSD, Darwin
 ARCH=$(uname -m) # x86_64, arm64, aarch64
-DISTRO=$( ([[ -e "/usr/bin/yum" ]] && echo 'CentOS') || ([[ -e "/usr/bin/apt" ]] && echo 'Debian') || echo 'unknown' )
 GITPROXY='https://ghproxy.com'
+
+# check linux release
+# 通过 /etc/os-release 文件判断发行版
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+    if [[ $ID = "debian" ]]; then
+        DISTRO="debian"
+    elif [[ $ID = "ubuntu" ]]; then
+        DISTRO="ubuntu"
+    elif [[ $ID = "centos" ]]; then
+        DISTRO="centos"
+    else
+        DISTRO="unknown"
+    fi
+# 通过 /etc/*-release 文件判断发行版
+elif [ -f /etc/centos-release ]; then
+    DISTRO="centos"
+elif [ -f /etc/redhat-release ]; then
+    DISTRO="redhat"
+elif [ -f /etc/fedora-release ]; then
+    DISTRO="fedora"
+elif [ -f /etc/debian_version ]; then
+    DISTRO="debian"
+elif [ -f /etc/lsb-release ]; then
+    DISTRO="ubuntu"
+else
+    DISTRO="unknown"
+fi
 
 _exit() {
     echo -e "${RED}Exiting...${NC}"
