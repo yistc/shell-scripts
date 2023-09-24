0761820ba9ead34eb1da2cd97b209ba853be85e1
yistc <32131698+yistc@users.noreply.github.com>
2023-09-25 01:45:23 +0800

    default value

--- lookbusy.sh
+++ lookbusy.sh
@@ -11,12 +11,23 @@ make install
 rm -rf /opt/lookbusy
 
 # ask user how much cpu to use
-echo "How much cpu do you want to use? (e.g. 25, 50, 75)"
+# default to 20
+echo "How much cpu do you want to use? (default 20)"
 read cpu
+if [ -z "$cpu" ]
+then
+    cpu=20
+fi
 
 # ask user how much memory to use
-echo "How much memory do you want to use? (e.g. 512MB, 1GB, 2GB)"
+# default 400MB
+echo "How much memory do you want to use? (e.g. 512MB, 1GB, 2GB, default 400MB)"
 read memory
+
+if [ -z "$memory" ]
+then
+    memory='400MB'
+fi
 
 cat > /etc/systemd/system/lookbusy.service << EOF
 [Unit]
