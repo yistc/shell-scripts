8ba079b0d985300aac538ca15e10d7a1d55a001e
yistc <32131698+yistc@users.noreply.github.com>
2023-10-09 18:31:33 +0800

    feat(nginx)

--- install/nginx.sh
+++ install/nginx.sh
@@ -51,6 +51,107 @@ _exit() {
     exit 1
 }
 
-debian_install() {
+example_conf() {
+cat >> /etc/nginx/conf.d/example <<EOF
+server {
+    listen 80;
+    server_name your.domain.com;
+    return 301 https://your.domain.com\$request_uri;
+}
+
+server {
+    listen 127.0.0.1:10001 ssl http2;
+
+    ssl_certificate /etc/nginx/certs/your.domain.cer;
+    ssl_certificate_key /etc/nginx/certs/your.domain.key;
+    ssl_session_timeout 1d;
+    ssl_session_cache shared:MozSSL:10m;  # about 40000 sessions
+    ssl_session_tickets off;
     
+    client_max_body_size 2G;
+
+    ssl_dhparam /etc/nginx/dhparam.pem;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+    ssl_prefer_server_ciphers off;
+
+    ssl_stapling on;
+    ssl_stapling_verify on;
+
+    resolver 8.8.8.8 1.1.1.1 8.8.4.4 valid=600s;
+    resolver_timeout 10s;
+    
+    location / {
+        proxy_pass http://127.0.0.1:8000;
+
+        # websocket
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "Upgrade";
+
+        proxy_set_header Host \$http_host;
+
+        proxy_set_header X-Forwarded-Proto \$scheme;
+
+        proxy_set_header X-Real-IP \$remote_addr;
+
+        # timeout
+        proxy_connect_timeout 300;
+        proxy_send_timeout 300;
+    	proxy_read_timeout 300;
+    	send_timeout 300;
+
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+
+    }
+
+}
+EOF
+}
+
+default_conf() {
+cat >/etc/nginx/conf.d/default.conf<<EOF
+server {
+    listen 80 default;
+    server_name _;
+    return 444; # or 500
+}
+EOF
+}
+
+ubuntu_install() {
+    release=$(lsb_release -cs)
+    curl -fSsL https://nginx.org/keys/nginx_signing.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+    # verify
+    gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+    # stable version
+    echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+
+    # prefer nginx from nginx.org
+    echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx
+
+    apt update -y
+    apt install nginx -y
+
+    cat >> /etc/nginx/nginx.conf <<EOF
+stream {
+    map \$ssl_preread_server_name \$name {
+        1.your.domain service1;
+    }
+    upstream service1 {
+        server 127.0.0.1:10001;
+    }
+
+    server {
+        listen 443 reuseport;
+        proxy_pass      \$name;
+        ssl_preread on;               # enalbe ssl_preread
+    }
+}
+EOF
+
+# example
+example_conf
+default_conf
 }
