80f022de752cf115868e685a2aaad6df4b1286e5
yistc <32131698+yistc@users.noreply.github.com>
2023-12-15 21:36:56 +0800

    Add locale configuration for en_US.UTF-8

--- init/ubuntu_amd64_init.sh
+++ init/ubuntu_amd64_init.sh
@@ -291,6 +291,7 @@ curl -LO https://raw.githubusercontent.com/yistc/shell-scripts/main/config/vim.s
 rm vim.sh
 
 # locale
+# take effect after reboot
 sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 update-locale LANG=en_US.UTF-8
