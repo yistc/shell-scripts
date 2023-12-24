f7294a08845bcf6916ca9b88e7f2a3110c14b47a
yistc <32131698+yistc@users.noreply.github.com>
2023-12-24 17:24:26 +0800

    Update Docker installation script

--- install/docker.sh
+++ install/docker.sh
@@ -16,7 +16,6 @@ NC='\033[0m' # No Color
 
 OS=$(uname -s) # Linux, FreeBSD, Darwin
 ARCH=$(uname -m) # x86_64, arm64, aarch64
-GITPROXY='https://ghproxy.com'
 
 # check linux release
 # 通过 /etc/os-release 文件判断发行版
@@ -50,22 +49,17 @@ _exit() {
     echo -e "${RED}Exiting...${NC}"
     exit 1
 }
-
-debian_install() {
-apt remove docker docker-engine docker.io containerd runc
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 apt update -y
-apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update -y
 apt install docker-ce docker-ce-cli containerd.io -y
@@ -85,57 +79,5 @@ cat >/etc/docker/daemon.json<<EOF
 EOF
 
 systemctl restart docker
-}
 
-ubuntu_install() {
-    apt remove docker docker-doc podman-docker docker-engine docker.io containerd runc
-    apt install ca-certificates curl gnupg -y
-
-    install -m 0755 -d /etc/apt/keyrings
-
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-    chmod a+r /etc/apt/keyrings/docker.gpg
-
-    echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-  apt update -y
-
-  apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
-
-  curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
-
-  docker-compose -v
-
-  # limiting log size
-  cat > /etc/docker/daemon.json << EOF
-  {
-    "log-driver": "json-file",
-    "log-opts": {
-      "max-size": "5m"
-    }
-  }
-EOF
-
-  systemctl restart docker
-}
-
-# install docker
-# if centos, warn user and exit 1
-
-if [[ $DISTRO == 'centos' ]]; then
-    echo -e "${RED}CentOS is not supported!${NC}"
-    exit 1
-elif [[ $DISTRO == 'debian' ]]; then
-    debian_install
-elif [[ $DISTRO == 'ubuntu' ]]; then
-    ubuntu_install
-else
-    echo -e "${RED}Unknown distro!${NC}"
-    exit 1
-fi
-
-rm docker.sh
+rm -f $0
