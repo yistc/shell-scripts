954fa16b92687cc00379f815adad4d6d9710aa74
yistc <32131698+yistc@users.noreply.github.com>
2024-01-20 16:31:12 +0800

    Add: pyenv

---
+++ install/pyenv.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+[[ $EUID -ne 0 ]] && echo "Error: This script must be run as root!" && exit 1
+
+trap _exit INT QUIT TERM
+
+# Colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+
+OS=$(uname -s) # Linux, FreeBSD, Darwin
+ARCH=$(uname -m) # x86_64, arm64, aarch64
+DISTRO=$( ([[ -e "/usr/bin/yum" ]] && echo 'CentOS') || ([[ -e "/usr/bin/apt" ]] && echo 'Debian') || echo 'unknown' )
+
+_exit() {
+    echo -e "${RED}Exiting...${NC}"
+    exit 1
+}
+
+echo -e "${PURPLE}Installing pyenv and CPython${NC}"
+
+# Install dependencies
+apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+git clone https://github.com/pyenv/pyenv.git /usr/local/bin/pyenv
+
+echo 'export PYENV_ROOT="/usr/local/bin/pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+echo 'export PYENV_ROOT="/usr/local/bin/pyenv"' >> ~/.zprofile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
+echo 'eval "$(pyenv init -)"' >> ~/.zprofile
+
+echo 'export PYENV_ROOT="/usr/local/bin/pyenv"' >> ~/.zlogin
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zlogin
+echo 'eval "$(pyenv init -)"' >> ~/.zlogin
+
+# pyenv virtualenv plugin
+git clone https://github.com/pyenv/pyenv-virtualenv.git /usr/local/bin/pyenv/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+
+# pyenv update plugin
+git clone https://github.com/pyenv/pyenv-update.git /usr/local/bin/pyenv/plugins/pyenv-update
+
+export PYENV_ROOT="/usr/local/bin/pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
