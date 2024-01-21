3f05720cfc7a24b0a69b25454b36deb4de3dc277
yistc <32131698+yistc@users.noreply.github.com>
2024-01-21 16:53:35 +0800

    Fix

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -23,7 +23,7 @@ _exit() {
     exit 1
 }
 
-echo -e "${PURPLE}Installing pyenv and CPython${NC}"
+echo -e "${PURPLE}Installing pyenv${NC}"
 
 # Install dependencies
 apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
