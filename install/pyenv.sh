6faa10c75fe1cdbeb738df34379016f4b9b1fc82
yistc <32131698+yistc@users.noreply.github.com>
2024-01-21 17:35:25 +0800

    Update

--- install/pyenv.sh
+++ install/pyenv.sh
@@ -26,7 +26,7 @@ _exit() {
 echo -e "${PURPLE}Installing pyenv${NC}"
 
 # Install dependencies
-apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 git clone https://github.com/pyenv/pyenv.git /usr/local/bin/pyenv
 
