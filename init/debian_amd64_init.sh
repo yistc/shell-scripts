07d8c7d629edf6a87552f93fdfc5b5a9f252a14e
yistc <32131698+yistc@users.noreply.github.com>
2024-08-02 16:33:00 +0800

    Refactor ssh key setup in init scripts

--- init/debian_amd64_init.sh
+++ init/debian_amd64_init.sh
@@ -110,8 +110,6 @@ systemctl restart systemd-journald
 # ssh keys
 mkdir -p /root/.ssh
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP0kVbDmjFhOtyoli41xVYMqok5zQNWUkYbdHBvVpAb9 yistc' >> ~/.ssh/authorized_keys
-# servercat key
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJ+sdnCybIStkqj/lNE8LLu5EPtUwRHmMquMLJgT6RP' >> ~/.ssh/authorized_keys
 
 # permit root login and disable password login
 # sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
