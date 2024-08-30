18c4d616f5767f5f99614edac8d3293e0760e292
yistc <32131698+yistc@users.noreply.github.com>
2024-08-30 12:31:20 +0800

    chore: update snell

--- network/snell.sh
+++ network/snell.sh
@@ -14,16 +14,15 @@ NC='\033[0m' # No Color
 ARCH=$(uname -m) # x86_64, arm64, aarch64
 
 if [[ "$ARCH" == "x86_64" ]]; then
-    curl -LO https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip
-    unzip snell-server-v4.0.1-linux-amd64.zip
+    curl -L https://dl.nssurge.com/snell/snell-server-v4.1.0rc1-linux-amd64.zip -o snell.zip
+    unzip snell.zip
     chmod +x snell-server && mv snell-server /usr/local/bin/snell
-    rm snell-server-v4.0.1-linux-amd64.zip
-
+    rm snell.zip
 elif [[ "$ARCH" == "arm64" || "$ARCH" == "aarch64" ]]; then
-    curl -LO https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-aarch64.zip
-    unzip snell-server-v4.0.1-linux-aarch64.zip
+    curl -L https://dl.nssurge.com/snell/snell-server-v4.1.0rc1-linux-aarch64.zip -o snell.zip
+    unzip snell.zip
     chmod +x snell-server && mv snell-server /usr/local/bin/snell
-    rm snell-server-v4.0.1-linux-aarch64.zip
+    rm snell.zip
 else
     echo "Error: This script only supports x86_64, arm64 and aarch64."
     exit 1
@@ -40,6 +39,7 @@ read PORT
 cat > /etc/snell/snell.conf <<EOF
 [snell-server]
 listen = 0.0.0.0:$PORT
+dns = 8.8.8.8, 1.1.1.1, 2001:4860:4860::8888
 psk = $PSK
 ipv6 = false
 EOF
