2471008e980fe138ba31cf89cdb2ce10182ed5bb
yistc <32131698+yistc@users.noreply.github.com>
2024-10-16 02:32:42 +0800

    feat: docker ipv6

--- install/docker.sh
+++ install/docker.sh
@@ -13,9 +13,43 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 NC='\033[0m' # No Color
+V4PROXY="http://127.0.0.1:8901"
 
 OS=$(uname -s) # Linux, FreeBSD, Darwin
 ARCH=$(uname -m) # x86_64, arm64, aarch64
+
+# Check if ipv6-only
+
+GOOGLE_IPV4_ADDRESS="8.8.8.8"
+GOOGLE_IPV6_ADDRESS="2001:4860:4860::8888"
+ping_ipv4() {
+    ping -4 -c 3 -W 2 "$GOOGLE_IPV4_ADDRESS" > /dev/null 2>&1
+    return $?
+}
+
+ping_ipv6() {
+    ping -6 -c 3 -W 2 "$GOOGLE_IPV6_ADDRESS" > /dev/null 2>&1
+    return $?
+}
+
+# Ping IPv4
+ping_ipv4
+IPV4_STATUS=$?
+
+# Ping IPv6
+ping_ipv6
+IPV6_STATUS=$?
+
+IP_STATUS=""
+if [ $IPV4_STATUS -eq 0 ] && [ $IPV6_STATUS -eq 0 ]; then
+    IP_STATUS="dual"
+elif [ $IPV4_STATUS -eq 0 ] && [ $IPV6_STATUS -ne 0 ]; then
+    IP_STATUS="v4-only"
+elif [ $IPV4_STATUS -ne 0 ] && [ $IPV6_STATUS -eq 0 ]; then
+    IP_STATUS="v6-only"
+else
+    IP_STATUS="none"
+fi
 
 # check linux release
 # 通过 /etc/os-release 文件判断发行版
@@ -65,7 +99,14 @@ apt update -y
 apt install docker-ce docker-ce-cli containerd.io -y
 docker -v
 
-curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+# download
+if [[ $IP_STATUS == "v6-only" ]]; then
+  curl -x $V4PROXY -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+else
+  curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+fi
+
+chmod +x /usr/local/bin/docker-compose
 docker-compose -v
 
 # limiting log size
