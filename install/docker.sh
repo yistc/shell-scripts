f6ee451efb0a3fa32ef7e7a7ef63ac82f34519bd
yistc <32131698+yistc@users.noreply.github.com>
2025-01-05 01:23:42 +0800

    fix: docker compose

--- install/docker.sh
+++ install/docker.sh
@@ -96,7 +96,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update -y
-apt install docker-ce docker-ce-cli containerd.io -y
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 docker -v
 
 # # docker compose
